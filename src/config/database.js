727b210c2cdf996ae89b69fcfc69599871eda034
mmaunze <mlmaunze@gmail.com>
2025-06-27 17:29:08 +0200

    config

--- src/config/database.js
+++ src/config/database.js
@@ -9,12 +9,12 @@ const sequelize = process.env.NODE_ENV === 'test' ?
     logging: false
   }) :
   new Sequelize(
-    process.env.DB_NAME || 'portfolio_db',
-    process.env.DB_USER || 'portfolio_user',
-    process.env.DB_PASSWORD || 'portfolio_password',
+    process.env.DB_NAME, //|| 'portfolio_db',
+    process.env.DB_USER, //|| 'portfolio_user',
+    process.env.DB_PASSWORD, //|| 'portfolio_password',
     {
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 3306, 
+      host: process.env.DB_HOST, // || 'localhost',
+      port: process.env.DB_PORT, // || 3306,
       dialect: 'mysql',
       logging: process.env.NODE_ENV === 'development' ? console.log : false,
       pool: {
